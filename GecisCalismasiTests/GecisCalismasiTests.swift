e5c9b0ab2f0509849ea9f3c08d79acbb7e0a30cb
bekoszn <berkeozguder@gmail.com>
2024-09-23 16:54:25 +0300

    Initial Commit

---
+++ GecisCalismasiTests/GecisCalismasiTests.swift
@@ -0,0 +1,17 @@
+//
+//  GecisCalismasiTests.swift
+//  GecisCalismasiTests
+//
+//  Created by Berke Özgüder on 23.09.2024.
+//
+
+import Testing
+@testable import GecisCalismasi
+
+struct GecisCalismasiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
