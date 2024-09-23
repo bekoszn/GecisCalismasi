efcf3f1dfcbe8eed3fd57d3fb4101c5dd0ccb418
bekoszn <berkeozguder@gmail.com>
2024-09-23 17:22:23 +0300

    Dorduncu odevimi paylasiyorum

--- GecisCalismasi/ViewController.swift
+++ GecisCalismasi/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func geriButonu(_ sender: Any) {
+        performSegue(withIdentifier: "ViewController", sender: nil)
+    }
+    
 }
 
